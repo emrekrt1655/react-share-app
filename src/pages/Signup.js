eb7b59a76e3deb7bb42da2a12f4ce06fd2ee8c17
Emre <emrekurtt1655@gmail.com>
2020-11-26 21:29:24 +0100

    Add formik

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,22 +1,62 @@
 import React from "react";
 import { Button, Grid, TextField, Container
  } from "@material-ui/core";
+ import {makeStyles} from '@material-ui/core/styles';
+ import {useFormik} from 'formik';
+
+ const styles = makeStyles({
+     wrapper:{
+         marginTop: '5rem'
+     }
+ })
 
 function Signup() {
+    const formik = useFormik({
+        initialValues: {
+          displayName:'',
+          email: '',
+          password:'',
+        },
+        onSubmit: values => {
+          alert(JSON.stringify(values, null, 2));
+        },
+      });
+    const signupStyles =  styles(); //we can take as object to take the codes as signupStyles.wrapper
   return (
-    <Container maxWidth='sm'>
+    <Container maxWidth='sm' className={signupStyles.wrapper}>
+        <form onSubmit={formik.handleSubmit}>
       <Grid container spacing ={3}>
           <Grid item xs = {12}>
-      <TextField id="outlined-basic" label="Display Name" variant="outlined" fullWidth />
+      <TextField 
+      name='displayName' 
+      label="Display Name" 
+      variant="outlined" 
+      fullWidth 
+      value={formik.values.displayName} 
+      onChange={formik.handleChange}/>
       </Grid>
       <Grid item xs = {12}>
-      <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth />
+      <TextField
+      name='email'
+      label="Email"
+      variant="outlined" 
+      fullWidth
+      value={formik.values.email} 
+      onChange={formik.handleChange}
+       />
       </Grid>
       <Grid item xs = {12}>
-      <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth />
+      <TextField 
+      name='password' 
+      label="Password" 
+      variant="outlined" 
+      type='password'
+      fullWidth
+      value={formik.values.password} 
+      onChange={formik.handleChange} />
       </Grid>
       <Grid item xs = {12}>
-      <Button variant="contained" color="primary" fullWidth>
+      <Button type='submit' variant="contained" color="primary" fullWidth>
         Submit
       </Button>
       </Grid>
@@ -26,6 +66,7 @@ function Signup() {
       </Button>
       </Grid>
       </Grid>
+      </form>
     </Container>
   );
 }
