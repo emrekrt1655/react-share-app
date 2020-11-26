1f79119e62bad8161e5e72dfe72f99d4981c9e55
Emre <emrekurtt1655@gmail.com>
2020-11-26 22:38:39 +0100

    navbar creeated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Signup from './pages/Signup';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Signup/>
     </div>
   );
