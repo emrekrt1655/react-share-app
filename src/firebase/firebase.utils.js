4e674bc9752220ffafeead379502891ed7309cc4
Emre <emrekurtt1655@gmail.com>
2020-11-29 05:09:42 +0100

    add try&catch

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -23,13 +23,19 @@ const devConfig = {
       this.firebaseAuth = firebase.auth();
     }
 
-    async register(displayName, email,password) {
-      await this.firebaseAuth.createUserWithEmailAndPassword(email, password);
-
-      this.firebaseAuth.currentUser.updateProfile({
-        displayName,
-      })
-    };
+    
+      async register(displayName, email,password) {
+        try {
+          await this.firebaseAuth.createUserWithEmailAndPassword(email, password);
+  
+          this.firebaseAuth.currentUser.updateProfile({
+            displayName,
+          });
+        } catch (err) {
+          console.log('F. Error', err);
+        }
+      };
+   
 
     useGoogleProvider () {
       const googleProvider = new firebase.auth.GoogleAuthProvider();
