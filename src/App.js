cb88584b145197823b5cd2a402efa9f7c8375317
Emre <emrekurtt1655@gmail.com>
2020-11-29 03:20:04 +0100

    authcontext created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import AppRouter from './Router.js/Router'
+import AuthContextProvider from './context/AuthContext'
 
 function App() {
   
   return (
     <div className="App">
+     <AuthContextProvider>
      <AppRouter/>
+     </AuthContextProvider>
     </div>
   );
 }
---
+++ src/context/AuthContext.js
@@ -0,0 +1,26 @@
+import { PinDropSharp } from '@material-ui/icons';
+import {createContext, useState, useEffect} from 'react';
+import firebase from '../firebase/firebase.utils';
+
+
+const FirebaseAuthContext = createContext();
+
+const AuthContextProvider = (props) => {
+    const [isAuthenticated, setAuthenticated] = useState(false);
+    const [currentUser, setCurrentUser] = useState();
+
+    useEffect(()=>{
+        firebase.firebaseAuth.onAuthStateChanged((user)=>{
+            console.log('user', user);
+            setCurrentUser(user);
+        })
+    })
+
+    return(
+        <FirebaseAuthContext.Provider value={{currentUser}}>
+            {props.children}
+        </FirebaseAuthContext.Provider>
+    );
+};
+
+export default AuthContextProvider;
